1a17d411d5abdd2f77030fc16f20f60587286faf
Shahzad <Farha@Farha-PC>
2019-04-21 20:18:30 +0530

    666

--- src/main/java/com/test/Jenkins.java
+++ src/main/java/com/test/Jenkins.java
@@ -5,7 +5,7 @@ public class Jenkins {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("Hello 555");
+		System.out.println("Hello 666");
 	}
 
 }
